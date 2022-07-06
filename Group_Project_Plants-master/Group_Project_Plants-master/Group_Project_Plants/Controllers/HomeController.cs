8257723ae647cb58c02041dd5284a4efa83db3a8
tlastelick1 <75869801+tlastelick1@users.noreply.github.com>
2022-07-06 18:20:56 -0500

    Add files via upload

---
+++ Group_Project_Plants-master/Group_Project_Plants-master/Group_Project_Plants/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Group_Project_Plants.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Group_Project_Plants.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Group_Project_Plants-master/Group_Project_Plants-master/Group_Project_Plants/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Group_Project_Plants.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
